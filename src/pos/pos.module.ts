67cee28cebd7866cb8eb012167a38edf4b0a5375
HorizUntall <rayuntal0112@gmail.com>
2026-08-28 00:46:13 +0800

    fix

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PosService } from './pos.service';
 import { PosController } from './pos.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { InventoryModule } from 'src/inventory/inventory.module';
 
 @Module({
-  imports: [PrismaModule, PosModule],
+  imports: [PrismaModule, InventoryModule],
   providers: [PosService],
   controllers: [PosController],
 })
